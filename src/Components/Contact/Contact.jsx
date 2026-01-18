2216eb1a5d3299b2ee1c7f6ded5856fa1fd8466c
Sebastian Navarro <sebastnavarro@gmail.com>
2026-01-18 04:04:20 -0300

    Adding functionality

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,8 +6,34 @@ import call_icon from '../../assets/call_icon.svg'
 
 
 const Contact = () => {
+
+    const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "28829a8a-7528-4655-bde1-e7eb40ca51c1");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert(res.message);
+    }
+  };
+
+
+
     return (
-        <div className='contact'>
+        <div id='contact' className='contact'>
             <div className="contact-title">
                 <h1>Get in touch</h1>
                 <img src={theme_pattern} alt="" />
@@ -28,7 +54,7 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
-                <form className="contact-right">
+                <form onSubmit={onSubmit} className="contact-right">
                     <label htmlFor="">Your Name</label>
                     <input type="text" placeholder='Enter your name' name='name' />
                     <label htmlFor="">Your Email</label>
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
     return (
-        <div className='hero'>
+        <div id='home' className='hero'>
             <img src={profile_img} alt="" />
             <h1>Hola! Soy <span>Sebastian Navarro,</span> Desarrollador Fullstack radicado en Santiago de Chile. </h1>
             <p>Desarrollador Full Stack con base sólida en front-end y fuerte orientación al aprendizaje continuo. Soy autodidacta,
@@ -12,7 +13,7 @@ const Hero = () => {
                 Actualmente enfocado en capacitarme en áreas como inteligencia artificial y cloud computing, buscando crecer en
                 entornos donde pueda seguir desarrollándome y aportar valor real enfrentando nuevos desafíos.</p>
             <div className="hero-action">
-                <div className="hero-connect">Connect With Me</div>
+                <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
                 <div className="hero-resume">My Resume</div>
             </div>    
         </div>
