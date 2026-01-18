ab1af2f49d9d239b5bcc91b1832a4484e80af56c
Sebastian Navarro <sebastnavarro@gmail.com>
2026-01-18 03:28:31 -0300

    Footer section finished

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,35 @@
 import './Footer.css'
+import footer_logo from '../../assets/footer_logo.svg'
+import user_icon from '../../assets/user_icon.svg'
+
 
 const Footer = () => {
-  return (
-    <div>
-
-
-    </div>
-  )
+    return (
+        <div className='footer'>
+            <div className="footer-top">
+                <div className="footer-top-left">
+                    <img src={footer_logo} alt="" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In est architecto accusantium voluptate blanditiis impedit dicta illum? Cupiditate aliquam tenetur ad sapiente officiis eum necessitatibus perspiciatis praesentium. Quia, quae obcaecati.</p>
+                </div>
+                <div className="footer-top-right">
+                    <div className="footer-email-input">
+                        <img src={user_icon} alt="" />
+                        <input type="email" placeholder='Enter your email' />
+                    </div>
+                    <div className="footer-subscribe">Subscribe</div>
+                </div>
+            </div>
+            <hr />
+            <div className="footer-bottom">
+                <p className="footer-bottom-left">fgvadf asdfsadf asdfasdfasdfa sdf</p>
+                <div className="footer-bottom-right">
+                    <p>Term of Services</p>
+                    <p>Privacy Policy</p>
+                    <p>Connect with me</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
