c62263f389b5583b315cae1a901208476c08f906
Sebastian Navarro <sebastnavarro@gmail.com>
2026-01-19 02:35:12 -0300

    Fixing problem to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar.jsx';
 import Hero from './Components/Hero/Hero.jsx';
 import About from './Components/About/About.jsx';
-import Services from './Components/Services/services.jsx';
+import Services from './Components/Services/Services.jsx';
 import MyWork from './Components/MyWork/MyWork.jsx';
 import Contact from './Components/Contact/Contact.jsx';
 import Footer from './Components/Footer/Footer.jsx';
